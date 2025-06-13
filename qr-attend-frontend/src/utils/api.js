ad952bb52fb89ca8b2f6d6f153485f815c53ec8f
Binit870 <binitpatra870@gmail.com>
2025-06-13 13:41:05 +0530

    Updated API base URL to Render backend

--- qr-attend-frontend/src/utils/api.js
+++ qr-attend-frontend/src/utils/api.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api', // ✅ hardcoded base URL
+  baseURL: 'https://qr-code-backend-10qs.onrender.com/api', // ✅ use your Render backend
 });
+
 
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem('token');
